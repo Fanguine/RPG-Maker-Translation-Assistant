24263996bea69900a21495c24db99827975287dd
Fanguine <fanguine@protonmail.com>
2020-12-17 22:26:41 +0100

    Improve UI

--- src/main.js
+++ src/main.js
@@ -2,27 +2,60 @@ const { app, dialog, ipcMain } = require('electron');
 const fs = require('fs');
 const Window = require('./window.js');
 
+let mainWindow = null;
+let gameFolderPath = null;
+let dataFolderPath = null;
+
+ipcMain.on('minimizeProgram', (event) => {
+    mainWindow.minimize();
+});
+
+ipcMain.on('maximizeProgram', (event) => {
+    const windowIsMaximized = mainWindow.isMaximized();
+    if (windowIsMaximized) {
+        mainWindow.unmaximize();
+    } else {
+        mainWindow.maximize();
+    }
+    event.sender.send('setMaximizeWindowIcon', windowIsMaximized);
+});
+
+ipcMain.on('closeProgram', (event) => {
+    app.quit();
+});
+
 ipcMain.on('selectGameFolder', (event) => {
-    const gameFolderPath = dialog.showOpenDialogSync({ properties: ['openDirectory', 'dontAddToRecent'] });
+    gameFolderPath = dialog.showOpenDialogSync({ properties: ['openDirectory', 'dontAddToRecent'] });
     if (gameFolderPath) {
-        const dataFolderPath = gameFolderPath[0] + '/www/data';
+        dataFolderPath = gameFolderPath[0] + '/www/data';
         if (fs.existsSync(dataFolderPath)) {
             const dataFilePaths = fs.readdirSync(dataFolderPath);
             if (dataFilePaths.length > 0) {
-                event.sender.send('setGameFolderPathLabel', gameFolderPath);
+                event.sender.send('setGameFolderPathLabel', "Selected game folder: " + gameFolderPath);
             }
         }
     }
 });
 
+ipcMain.on('removeGameFolder', (event) => {
+    gameFolderPath = null;
+    dataFolderPath = null;
+});
+
+ipcMain.on('decryptDataFiles', (event) => {
+    const decryptionKey = getDecryptionKey();
+    console.log(decryptionKey);
+});
+
+
+function getDecryptionKey() {
+
+};
+
 function main () {
-    const mainWindow = new Window({
+    mainWindow = new Window({
         file: './public/index.html'
     });
 };
 
 app.whenReady().then(main);
-
-app.on('window-all-closed', () => {
-    app.quit();
-});
